e16c0813ca070693c7a45aec358373fbc7d56ee2
FiveDomain <krulgpl@wp.pl>
2019-08-23 17:52:50 +0200

    First shader

--- TEST/TEST/main.cpp
+++ TEST/TEST/main.cpp
@@ -1,16 +1,47 @@
-
-#include <glad/glad.h>
-//#include <GL/glew.h>
+#include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-#pragma comment(lib, "glew32.lib")
-#pragma comment(lib, "freeglut.lib")
-
-
 using namespace std;
+
+static unsigned int CompileShader( unsigned int type, const std::string& sourse) {
+	unsigned int id = glCreateShader(type);
+	const char* src = sourse.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		int lenght;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &lenght);
+		char* message = static_cast<char*>(alloca(lenght * sizeof(char)));
+		glGetShaderInfoLog(id, lenght, &lenght, message);
+		cout << "failed to compile shader!" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << endl;
+		cout << message << endl;
+		glDeleteShader(id);
+		return 0;
+	}
+	return id;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+	
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
 
 //wymiary obrazu
 const int WIDTH = 1280;
@@ -19,7 +50,7 @@ const int HEIGHT = 960;
 //inicjalizacja obiektu OpenGL 
 void OnInit() {
 	//ustawienie koloru czerwonego
-	glClearColor(0.64f, 0.81f, 0.182f, 0);
+	glClearColor(0.3f, 0.8f, 0.4f, 0);
 	cout << "Inicjalizacja powiod³a siê" << endl;
 }
 
@@ -44,21 +75,8 @@ void OnRender() {
 
 int main(int argc, char** argv) {
 
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-
-	return 0;
-
-	/*
+	
 	//inicjuj¹ce wywo³ania biblioteki freeglut
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
@@ -91,13 +109,38 @@ int main(int argc, char** argv) {
 	//inicjalizacja OpenGL
 	OnInit();
 
+	std::string vertexShader =
+		"#version 330 core"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main() {\n"
+		"gl_Position = position;\n"
+		"}\n"
+		"\n";
+	std::string fragmentShader =
+		"#version 330 core"
+		"\n"
+		"layout(location = 0) out vec4 color; \n"
+		"\n"
+		"void main() {\n"
+		"color = vec4(1.0, 0.0, 0.0, 1.0); \n"
+		"}\n"
+		"\n";
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+
+	glUseProgram(shader);
+
 	//rejestracja funkcji zwrotnych
 	glutCloseFunc(OnShutdown);
 	glutDisplayFunc(OnRender);
 	glutReshapeFunc(OnResize);
 
+	//vec4(position.xy)
+
 	//uruchomienie g³ównej pêtli
 	glutMainLoop();
 
-	return 0;*/
+	return 0;
 }
